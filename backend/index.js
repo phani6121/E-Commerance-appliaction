8698569c7b4a9d850a90c58e8c1543b4930454b8
phani6121 <149896536+phani6121@users.noreply.github.com>
2025-12-04 13:07:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
